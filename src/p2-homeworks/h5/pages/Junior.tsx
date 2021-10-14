5f38b84eab26a39fa6f6108bed03354c1e5f3298
suragapavel <suraga.pasha@yandex.by>
2021-10-14 14:12:59 +0300

    h7

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HW6 from "../../h6/HW6";
+import HW7 from "../../h7/HW7";
 
 function Junior() {
     return (
         <div>
          <div>JUNIOR</div>
             <HW6/>
+            <HW7/>
         </div>
     )
 }
